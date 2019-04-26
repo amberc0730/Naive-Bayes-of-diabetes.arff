64ddad4a9e616fa363b5e18cce75b37765af96e0
amberc0730 <jyanjhou850912@gmail.com>
2019-04-26 12:02:31 +0800

    Update naive bayes.py

--- naive bayes/naive bayes.py
+++ naive bayes/naive bayes.py
@@ -1,37 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[15]:
-
-
 import pandas as pd
 #讀取CSV檔案
 data = pd.read_csv('diabetes.csv') 
 
-
-# In[16]:
-
-
 data.info()
 
-
-# In[17]:
-
-
 data.describe ( )
-
-
-# In[18]:
-
 
 #x:input
 x = data.loc[:,['preg','plas','pres','skin','insu','mass','pedi','age']]
 #y:output
 y = data.loc[:,['class']]
-
-
-# In[19]:
-
 
 from sklearn import preprocessing
 
@@ -58,10 +39,6 @@ feature=list(zip(X_preg_encoded, X_plas_encoded,X_pres_encoded,X_skin_encoded,X_
 import numpy as np
 features=np.asarray(feature)
 
-
-# In[20]:
-
-
 #Import Gaussian Naive Bayes 模型 (高斯樸素貝氏)
 from sklearn.naive_bayes import GaussianNB
 model = GaussianNB()
@@ -70,43 +47,12 @@ model = GaussianNB()
 # model.fit(x, y)
 model.fit(features, Y_class_label)
 
-
-# In[21]:
-
-
 expected = Y_class_label
 predicted = model.predict(features)
 from sklearn import metrics
 print(metrics.classification_report(expected, predicted))
 
-
-# In[22]:
-
-
 print(metrics.confusion_matrix(expected, predicted))
-
-
-# In[23]:
-
 
 predicted = model.predict([[2,1,0,0,2,1,2,20]])
 print ("Predicted Value:", predicted)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
